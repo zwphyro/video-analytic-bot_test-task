1251baad83374f5689987a1e1d15b5a65a58c77f
zwphyro <tofumaks@gmail.com>
2026-02-06 04:26:35 +0300

    add token and key

--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,9 @@ class Settings(BaseSettings):
     db_user: str = Field(..., alias="POSTGRES_USER")
     db_password: str = Field(..., alias="POSTGRES_PASSWORD")
 
+    openai_api_key: str = Field(..., alias="OPENAI_API_KEY")
+    tg_bot_token: str = Field(..., alias="TG_BOT_TOKEN")
+
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", ".env"),
         extra="ignore",
